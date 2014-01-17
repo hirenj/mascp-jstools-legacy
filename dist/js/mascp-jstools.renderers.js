175daad3e0881560ca2c7ec849d73d89cb88cdec
Hiren Joshi <hirenj@gmail.com>
2014-01-17 22:49:17 +0100

    Remove jQuery from sequenceChange
    Former-commit-id: 5eef49b3146d2c05e49156096df1b68d9bd3e6b9

--- dist/js/mascp-jstools.renderers.js
+++ dist/js/mascp-jstools.renderers.js
@@ -311,7 +311,7 @@ MASCP.SequenceRenderer = (function() {
             }
     //        this._container.style.width = '100%';
 
-            jQuery(this).bind('sequenceChange', function(e){
+            bean.add(this,'sequenceChange', function(e){
                 jQuery(sequenceContainer).text("");
                 jQuery(sequenceContainer).append(this._sequence_els);
                 jQuery(sequenceContainer).append(jQuery('<div style="clear: both; float: none; height: 0px; width: 100%;"></div>'));
@@ -437,7 +437,7 @@ MASCP.SequenceRenderer.prototype.setSequence = function(sequence)
         this._renderer = renderer;
     });
     this._sequence_els = sequence_els;   
-    jQuery(this).trigger('sequenceChange');
+    bean.fire(this,'sequenceChange');
 };
 
 /**
@@ -2292,9 +2292,9 @@ MASCP.CondensedSequenceRenderer = function(sequenceContainer) {
 
     // We want to unbind the default handler for sequence change that we get from
     // inheriting from CondensedSequenceRenderer
-    jQuery(this).unbind('sequenceChange');
+    bean.remove(this,'sequenceChange');
 
-    jQuery(this).bind('sequenceChange',function() {
+    bean.add(this,'sequenceChange',function() {
         for (var layername in MASCP.layers) {
             if (MASCP.layers.hasOwnProperty(layername)) {
                 MASCP.layers[layername].disabled = true;
@@ -2808,7 +2808,7 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
     clazz.prototype.setSequence = function(sequence) {
         var new_sequence = this._cleanSequence(sequence);
         if (new_sequence == this.sequence && new_sequence !== null) {
-            jQuery(this).trigger('sequenceChange');
+            bean.fire(this,'sequenceChange');
             return;
         }
     
@@ -2977,7 +2977,7 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
                 }
             });
 
-            jQuery(renderer).trigger('sequenceChange');
+            bean.fire(renderer,'sequenceChange');
         });
         var canvas = createCanvasObject.call(this);
         if (! this._canvas) {
@@ -2993,8 +2993,8 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
     
         var seq_change_func = function(other_func) {
             if ( ! rend._canvas ) {
-                rend.bind('sequenceChange',function() {
-                    jQuery(rend).unbind('sequenceChange',arguments.callee);
+                bean.add(rend,'sequenceChange',function() {
+                    bean.remove(rend,'sequenceChange',arguments.callee);
                     other_func.apply();
                 });
             } else {
@@ -3105,8 +3105,8 @@ MASCP.CondensedSequenceRenderer.prototype.addValuesToLayer = function(layerName,
     if ( ! canvas ) {        
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);
             orig_func.call(self,layerName,values);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3198,8 +3198,8 @@ var addElementToLayer = function(layerName,opts) {
     if ( ! canvas ) {        
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);            
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);            
             orig_func.call(self,layerName);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3310,8 +3310,8 @@ var addBoxOverlayToElement = function(layerName,width,fraction,opts) {
     if ( ! canvas ) {
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);            
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);            
             orig_func.call(self,layerName,width,opts);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3366,8 +3366,8 @@ var addTextToElement = function(layerName,width,opts) {
     if ( ! canvas ) {
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);
             orig_func.call(self,layerName,width,opts);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3413,8 +3413,8 @@ var addShapeToElement = function(layerName,width,opts) {
     if ( ! canvas ) {
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);
             orig_func.call(self,layerName,width,opts);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3488,8 +3488,8 @@ var addElementToLayerWithLink = function(layerName,url,width) {
     if ( ! canvas ) {
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);            
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);            
             orig_func.call(self,layerName,url,width);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3522,8 +3522,8 @@ var addCalloutToLayer = function(layerName,element,opts) {
     if ( ! canvas ) {
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);            
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);            
             orig_func.call(self,layerName,width,opts);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -3554,8 +3554,8 @@ var addAnnotationToLayer = function(layerName,width,opts) {
     if ( ! canvas ) {
         var orig_func = arguments.callee;
         var self = this;
-        this._renderer.bind('sequencechange',function() {
-            this._renderer.unbind('sequencechange',arguments.callee);            
+        bean.add(this._renderer,'sequencechange',function() {
+            bean.remove(this._renderer,'sequencechange',arguments.callee);            
             orig_func.call(self,layerName,width,opts);
         });
         log("Delaying rendering, waiting for sequence change");
@@ -4391,9 +4391,9 @@ clazz.prototype.addTrack = function(layer) {
     var renderer = this;
     
     if ( ! this._canvas ) {
-        this.bind('sequencechange',function() {
+        bean.add(this,'sequencechange',function() {
             this.addTrack(layer);
-            this.unbind('sequencechange',arguments.callee);
+            bean.remove(this,'sequencechange',arguments.callee);
         });
         console.log("No canvas, cannot add track, waiting for sequencechange event");
         return;
