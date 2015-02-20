8ea9973239f5432fb8216f64742311bc45fb7ffb
Hiren Joshi <hirenj@gmail.com>
2015-02-20 16:37:32 +0100

    Add custom element for editable data
    Former-commit-id: 5209d23dc214a96f25ff1d5c239660461fbe264b

--- dist/js/mascp-jstools.services.js
+++ dist/js/mascp-jstools.services.js
@@ -8727,53 +8727,8 @@ MASCP.UserdataReader.prototype.requestData = function()
 
 
 MASCP.UserdataReader.prototype.setupSequenceRenderer = function(renderer) {
-    var reader = this;
-    
-    var is_array = function(arr) {
-        return Object.prototype.toString.call(arr) == '[object Array]';
-    };
-    
-    reader.bind('resultReceived',function() {
-        var results = [].concat(this.result.data);
-        while(results.length > 0) {
-            var my_data = results.shift();
-            if ( ! my_data ) {
-                continue;
-            }
-            MASCP.registerLayer(reader.datasetname,{'fullname' : reader.datasetname,'color' : '#00ff00'});
-            var data_func = function() { return function(row) {
-                renderer.getAminoAcidsByPeptide(row).addToLayer(reader.datasetname);
-            }; }();
-            if (is_array(my_data) && (! (is_array(my_data[0])))) {
-                data_func = function() { return function(row) {
-                    var start = parseInt(row[0],10);
-                    var end = parseInt(row[1],10);
-                    if (! isNaN(start) && ! isNaN(end)) {
-                        renderer.getAA(start).addBoxOverlay(reader.datasetname,end-start);
-                    } else {
-                        row.forEach(function(cell) {
-                            renderer.getAminoAcidsByPeptide(cell).addToLayer(reader.datasetname);                            
-                        });
-                    }
-                }; }();
-            } else if (is_array(my_data) && ( is_array(my_data[0]) )) {
-                data_func = function() { return function(peps) {
-                    peps.forEach(function(row) {
-                        var start = parseInt(row[0],10);
-                        var end = parseInt(row[1],10);
-                        renderer.getAA(start).addBoxOverlay(reader.datasetname,end-start);
-                    });
-                }; }();                
-            } else if (my_data === parseInt(my_data[0],10)) {
-                data_func = function() { return function(row) {
-                    var pos = row;
-                    renderer.getAA(pos).addAnnotation(reader.datasetname,1);
-                }; }();
-            }
-            data_func.call(this,my_data);
-        }
-        renderer.trigger('resultsRendered',[reader]);        
-    });
+// We don't have any default rendering for the UserDataReader
+// since it's all going to be custom stuff anyway
 };
 
 (function() {
@@ -9810,7 +9765,7 @@ if (typeof document !== 'undefined' && 'registerElement' in document) {
           }
         },"xml");
       }
-      if (pref.type == 'liveClass') {
+      if (pref.type == 'liveClass' || pref.type == 'reader') {
         reader.registerSequenceRenderer(renderer,pref.render_options || {} );
       }
       var render_func = function() {
@@ -10053,6 +10008,47 @@ if (typeof document !== 'undefined' && 'registerElement' in document) {
         gatorReaderProto.attributeChangedCallback.apply(this);
       };
       document.registerElement('gator-localdata', { prototype: proto });
+      return proto;
+    })();
+
+    var editableReader = (function() {
+      var proto = Object.create( gatorReaderProto,{
+        'type' : {
+          get: function() { return "reader"; }
+        },
+        'config_id' : {
+          get: function() { return this._config_id; }
+        },
+        'data' : {
+          get: function() { return this._reader.data; },
+          set: function(data) {
+            this._reader.data = data;
+          }
+        }
+      });
+
+      var create_reader = function() {
+        var reader = new MASCP.EditableReader();
+        return reader;
+      };
+
+      proto.createdCallback = function() {
+        var self = this;
+        this._config_id = "editable-"+((new Date()).getTime());
+        gatorReaderProto.createdCallback.apply(this);
+        this._reader = create_reader.call(this);
+      };
+      proto._generateConfig = function() {
+        var config = gatorReaderProto._generateConfig.call(this);
+        config[this._config_id].reader = this._reader;
+        return config;
+      };
+
+      proto.attributeChangedCallback = function(attrName, oldVal, newVal) {
+        gatorReaderProto.attributeChangedCallback.apply(this);
+      };
+
+      document.registerElement('gator-editabledata', { prototype: proto });
       return proto;
     })();
 
