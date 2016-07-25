dbb1b4e0a962e55d9c60d25eaa0c61cd95dfdfe3
Hiren Joshi <hirenj@gmail.com>
2016-07-25 13:58:20 +0200

    Fix for layer methods
    Former-commit-id: 89385f871d1e2f1e2b3f65ce257561a7d664d0d7

--- dist/js/mascp-jstools.js
+++ dist/js/mascp-jstools.js
@@ -14579,11 +14579,13 @@ MASCP.registerLayer = function(layerName, options, renderers)
     }
 
     if (layer && options.group) {
-        layer.group = this.getGroup(options.group);
+        if (layer.group !== this.getGroup(options.group)) {
+            layer.group = this.getGroup(options.group);
+            layer.group._layers.push(layer);
+        }
         if ( ! layer.group ) {
             throw "Cannot register this layer with the given group - the group has not been registered yet";
         }
-        layer.group._layers.push(layer);
     }
     
     if (layer) {
@@ -20047,7 +20049,10 @@ MASCP.CondensedSequenceRenderer.Navigation = (function() {
             });
 
             bean.add(layer,'visibilityChange',function(rend,visible) {
-                if (group.size() > 0) {            
+                if (group.size() > 0) {
+                    if (! expanded_map.hasOwnProperty(layer.name)) {
+                        expanded_map[layer.name] = false;
+                    }
                     self.setGroupVisibility(group, expanded_map[layer.name] && visible,true);
                     renderer.refresh();
                 }
