3d888772a20fb37181b2082e1d3ddb155be21e85
Hiren Joshi <hirenj@gmail.com>
2016-08-01 14:13:29 +0200

    Track stretching
    Former-commit-id: 13a49538783f2ca78f0554a0f2d764e1e7216231

--- dist/js/mascp-jstools.js
+++ dist/js/mascp-jstools.js
@@ -16126,6 +16126,9 @@ var SVGCanvas = SVGCanvas || (function() {
                 container.setAttribute('width','200');
                 container.setAttribute('height','250');
             }
+            if (opts.angle) {
+                result.angle = opts.angle;
+            }
             // var rect = document.createElementNS(svgns,'rect');
             // rect.setAttribute('stroke','#f00');
             // rect.setAttribute('stroke-width','10');
@@ -18471,7 +18474,8 @@ MASCP.CondensedSequenceRenderer.prototype.renderObjects = function(track,objects
     }
     var results = [];
     objects.forEach(function(object) {
-        if (object.options && ((object.options.height || renderer._layer_containers[track].track_height) + object.options.offset) > renderer._layer_containers[track].track_height) {
+        var potential_height = object.options ? (object.options.height || renderer._layer_containers[track].track_height) + (object.options.offset + object.options.height || 0) : 0;
+        if (object.options && potential_height > renderer._layer_containers[track].track_height) {
             renderer._layer_containers[track].fixed_track_height = renderer._layer_containers[track].track_height + object.options.offset + (object.options.height || renderer._layer_containers[track].track_height);
         }
 
--- dist/js/mascp-jstools.renderers.js
+++ dist/js/mascp-jstools.renderers.js
@@ -3591,6 +3591,9 @@ var SVGCanvas = SVGCanvas || (function() {
                 container.setAttribute('width','200');
                 container.setAttribute('height','250');
             }
+            if (opts.angle) {
+                result.angle = opts.angle;
+            }
             // var rect = document.createElementNS(svgns,'rect');
             // rect.setAttribute('stroke','#f00');
             // rect.setAttribute('stroke-width','10');
@@ -5936,7 +5939,8 @@ MASCP.CondensedSequenceRenderer.prototype.renderObjects = function(track,objects
     }
     var results = [];
     objects.forEach(function(object) {
-        if (object.options && ((object.options.height || renderer._layer_containers[track].track_height) + object.options.offset) > renderer._layer_containers[track].track_height) {
+        var potential_height = object.options ? (object.options.height || renderer._layer_containers[track].track_height) + (object.options.offset + object.options.height || 0) : 0;
+        if (object.options && potential_height > renderer._layer_containers[track].track_height) {
             renderer._layer_containers[track].fixed_track_height = renderer._layer_containers[track].track_height + object.options.offset + (object.options.height || renderer._layer_containers[track].track_height);
         }
 
